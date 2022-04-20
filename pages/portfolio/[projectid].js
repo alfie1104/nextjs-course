03972a119171ca75fb9362884178f573e58e6e51
peter_alfie <alfie1104@gmail.com>
2022-04-20 22:46:09 +0900

    feat(clients folder): building Nested Dynamic Routes & Paths

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,9 +1,18 @@
 import { useRouter, withRouter } from "next/router";
 
 function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+  const projectId = router.query.projectid;
+
+  // send a request to some backend server
+  // to fetch the piece of data with an id of router.query.projectid
+
   return (
     <div>
-      <h1>The Portfolio Project Page</h1>
+      <h1>The Portfolio {projectId} Page</h1>
     </div>
   );
 }
