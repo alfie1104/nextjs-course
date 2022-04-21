228a1c6695b93bb28f70402847b0d3cd622d77f2
peter_alfie <alfie1104@gmail.com>
2022-04-22 08:53:07 +0900

    feat(events/index.js, event-search.js): navigating to the "Filtered Events" Page Progammatically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,21 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
-import { Fragment } from "react";
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
