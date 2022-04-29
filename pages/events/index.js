5a12e0cf19403d581a7b96e7aec5f59ffd4a0ceb
peter_alfie <alfie1104@gmail.com>
2022-04-29 15:47:38 +0900

    feat(api-utils, events/eventid, events/index, index): apply Pre-rendering & Data fetching

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
-import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
+import { getAllEvents } from "../../helpers/api-util";
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -19,6 +19,16 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
