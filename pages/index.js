ca4a168c8daf4e686b5438fdf7db596e4d0e7198
peter_alfie <alfie1104@gmail.com>
2022-05-02 21:55:05 +0900

    feat(*): add new branch (section8)

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,9 @@
-import Head from "next/head";
-import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../helpers/api-util";
-
 function HomePage(props) {
   return (
     <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta
-          name="description"
-          content="Find a lot of great events that allow you to evolve..."
-        />
-      </Head>
-      <EventList items={props.events} />
+      <h1>The Home Page</h1>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: featuredEvents,
-    },
-    revalidate: 1800,
-  };
 }
 
 export default HomePage;
