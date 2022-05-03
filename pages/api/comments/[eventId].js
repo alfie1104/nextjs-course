fabcb184e86afeafa637d0446a61f64db599f4da
peter_alfie <alfie1104@gmail.com>
2022-05-03 21:17:41 +0900

    feat(db-utils, [eventid], newsletter): inserting and getting comments using MongoDB

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,12 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+import { getAllDocuments } from "../../../helpers/db-util";
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  const url =
+    "mongodb+srv://nextjs:test@cluster0.xp3cg.mongodb.net/events?retryWrites=true&w=majority";
+  const client = await MongoClient.connect(url);
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -11,27 +18,35 @@ function handler(req, res) {
       text.trim() === ""
     ) {
       res.status(422).json({ message: "Invalid input." });
+      client.close();
       return;
     }
 
-    console.log(email, name, text);
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
 
-    console.log(newComment);
+    const db = client.db();
+    const result = await db.collection("comments").insertOne(newComment);
+
+    newComment.id = result.insertedId;
+
     res.status(201).json({ message: "Added comment.", comment: newComment });
   } else if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Max", text: "A first comment!" },
-      { id: "c2", name: "Manuel", text: "A second comment!" },
-    ];
+    const documents = await getAllDocuments(
+      client,
+      "comments",
+      { _id: -1 },
+      { eventId }
+    );
 
-    res.status(200).json({ comments: dummyList });
+    res.status(200).json({ comments: documents });
   }
+
+  client.close();
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -11,11 +11,11 @@ async function handler(req, res) {
     }
 
     const url =
-      "mongodb+srv://nextjs:test@cluster0.xp3cg.mongodb.net/newsletter?retryWrites=true&w=majority";
+      "mongodb+srv://nextjs:test@cluster0.xp3cg.mongodb.net/events?retryWrites=true&w=majority";
     const client = await MongoClient.connect(url);
     const db = client.db();
 
-    await db.collection("emails").insertOne({ email: userEmail });
+    await db.collection("newsletter").insertOne({ email: userEmail });
     client.close();
 
     res.status(201).json({ message: "Signed up!" });
